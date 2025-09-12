3c0a52628136a4e653f13b6c01d1c7e281489b06
Nach <nach.vidan@gmail.com>
2025-09-12 15:07:14 +0000

    make all the elments more smaller so ther will be more free spalce on th

--- src/app/log-day/page.tsx
+++ src/app/log-day/page.tsx
@@ -118,7 +118,7 @@ export default function LogDayPage() {
         </header>
 
         <main className="flex-1 p-4 md:p-6">
-            <div className="mx-auto w-full max-w-7xl">
+            <div className="mx-auto w-full max-w-6xl">
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
                     {/* Left Column */}
                     <div className="lg:col-span-2 space-y-6">
