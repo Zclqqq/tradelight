b064f9b8f60c99bd7b072d7dbf4a7d57f900f719
Nach <nach.vidan@gmail.com>
2025-09-16 19:56:56 +0000

    USE LORA FONT FOR THE WEBSITE

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['"Inter"', 'sans-serif'],
-        headline: ['"Playfair Display"', 'serif'],
+        body: ['"Lora"', 'serif'],
+        headline: ['"Lora"', 'serif'],
         code: ['monospace'],
       },
       colors: {
