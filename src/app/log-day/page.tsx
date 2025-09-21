ef3b43e26f3ec478f19e5dfcc6dc3b0bb2c1ad0a
Nach <nach.vidan@gmail.com>
2025-09-21 20:05:43 +0000

    Image Crop:  מלמעלה: בערך ‎40px (כדי להוריד את הפס האפור עם הטקסט).
    מלמ

--- src/app/log-day/page.tsx
+++ src/app/log-day/page.tsx
@@ -556,7 +556,7 @@ export default function LogDayPage() {
                                 <CardContent className="p-0 flex-1 flex flex-col relative">
                                     {analysisImage ? (
                                         <div className="w-full h-full relative overflow-hidden">
-                                            <div className="absolute left-0 right-0" style={{top: '0px', bottom: '-25px'}}>
+                                            <div className="absolute left-0 right-0" style={{top: '-40px', bottom: '-25px'}}>
                                                 <Image src={analysisImage} alt="Trade analysis" layout="fill" objectFit="cover" />
                                             </div>
                                             <Button
